cfb90d4362663190803d7c864d46e7f4baab6251
arc4n15 <arc4nis@outlook.com>
2019-03-15 16:45:20 -0400

    Private posts and sharing.

--- django/Assignment_2/paste/models.py
+++ django/Assignment_2/paste/models.py
@@ -2,21 +2,23 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django_cryptography.fields import encrypt
 
 
 class Post(models.Model):
-	title = encrypt(models.CharField(max_length=100))
-	content = encrypt(models.TextField())
+	title = models.CharField(max_length=100)
+	content = models.TextField()
 	date_posted = models.DateTimeField(default=timezone.now) #auto_now=True to update date to last modified
-	user = encrypt(models.ForeignKey(User, on_delete=models.CASCADE))
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	is_disabled = models.BooleanField(default=False)
 	expiry_date = models.DateTimeField(null=True, blank=True)
 	private = models.BooleanField(default=False)
-
+	permissioned_users = models.TextField(blank=True, null=True)
 
 	def __str__(self):
 		return self.title
 
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+	
